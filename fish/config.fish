742f83aaac721f1d44836174cac93522bf28517a
gitRaiku <sebi.pintilie@gmail.com>
2023-06-08 17:53:25 +0300

    Switch to ~

--- fish/config.fish
+++ fish/config.fish
@@ -13,14 +13,14 @@ export LANGUAGE=en_GB.UTF-8
 export LANG=en_GB.UTF-8
 export LC_ALL=en_GB.UTF-8
 export SDL_AUDIODRIVER=jack
-export GNULIB_SRCDIR=/home/arch/Git/gnulib
+export GNULIB_SRCDIR="~/Git/gnulib"
 export DEBUGINFOD_URLS="https://debuginfod.archlinux.org"
 
 export CFLAGS="-O3 -march=native -mtune=native -fmodulo-sched"
 export MAKEOPTS="-l16 -j16"
 export CPU_FLAGS_X86="aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt rdrand sse sse2 sse3 sse4_1 sse4_2 ssse3"
 
-source /home/arch/.config/fish/ls_colours
+source ~/.config/fish/ls_colours
 
 # set LS_COLORS (cat /home/arch/.config/ls_colours)
 # export LS_COLORS
