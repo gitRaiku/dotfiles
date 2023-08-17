80106281af45ea9b936b5f3778b0f19f441f0ea5
gitRaiku <sebi.pintilie@gmail.com>
2023-08-17 22:04:13 +0300

    muchi e gay

--- fish/config.fish
+++ fish/config.fish
@@ -6,12 +6,9 @@ if [ "$SSH_CLIENT" = "" ]
 end
 
 export TZ=Europe/Bucharest
-# export LANGUAGE=ja_JP.UTF-8
-# export LANG=ja_JP.UTF-8
-# export LC_ALL=ja_JP.UTF-8
-export LANGUAGE=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
+export LANGUAGE=ja_JP.UTF-8
+export LANG=ja_JP.UTF-8
+export LC_ALL=ja_JP.UTF-8
 export SDL_AUDIODRIVER=jack
 export GNULIB_SRCDIR="~/Git/gnulib"
 export DEBUGINFOD_URLS="https://debuginfod.archlinux.org"
@@ -19,6 +16,11 @@ export DEBUGINFOD_URLS="https://debuginfod.archlinux.org"
 export CFLAGS="-O3 -march=native -mtune=native -fmodulo-sched"
 export MAKEOPTS="-l16 -j16"
 export CPU_FLAGS_X86="aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt rdrand sse sse2 sse3 sse4_1 sse4_2 ssse3"
+
+export DISABLE_QT5_COMPAT=1
+export MOZ_ENABLE_WAYLAND=1
+export LIBSEAT_BACKEND=logind
+export WLR_NO_HARDWARE_CURSORS=1
 
 source ~/.config/fish/ls_colours
 
